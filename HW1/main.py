a432aa32aa109762e2696f0fbdcfe2724fe15435
Pavel Solikov <psolikov15@gmail.com>
2022-02-02 23:50:31 +0300

    Added hw

---
+++ HW1/main.py
@@ -0,0 +1,25 @@
+def fibonacci_n(n):
+    if n <= 0:
+        print(f"Bad")
+        return
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci_n(n - 1) + fibonacci_n(n - 2)
+
+
+def fibonacci_sequence(n):
+    seq = []
+    for i in range(1, n + 1):
+        seq.append(fibonacci_n(i))
+    return seq
+
+
+def print_ast(fun):
+    pass
+
+
+if __name__ == '__main__':
+    print(fibonacci_sequence(10))
